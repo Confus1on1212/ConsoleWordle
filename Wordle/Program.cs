510ab0f465f2b11f7e82f9f00f0fcf1ba04f6459
David <gamingdawe@gmail.com>
2024-05-22 19:58:56 +0200

    + ha nem talalja el 5 utan akkor is uj kor de 0 pont + nem txt hanem csv

--- Wordle/Program.cs
+++ Wordle/Program.cs
@@ -21,7 +21,7 @@ namespace Wordle
             // pontozas: negyedjere - 2 pont
             // pontozas: otodjere - 1 pont
             // pontozas: nem talalja ki ennyi proba utan akkor 0 pont -> uj szo uj kor
-            // 3 kor utan kiirja az reszpontokat / kor + osszpontot , talan leaderboard
+            // 3 kor utan kiirja az osszpontot 
 
             JatekMenu();
             Console.ReadKey();
@@ -31,7 +31,6 @@ namespace Wordle
             Console.Clear();
             Console.WriteLine("Főmenü");
             Console.WriteLine("1. Játék Kezdete \t 2. Előző Pontszámok Megtekintése \t 3. Vissza a Főmenübe");
-            // Add toggle admin menu !!!!!!
 
             switch (Console.ReadKey(true).Key)
             {
@@ -55,7 +54,6 @@ namespace Wordle
         private static void ElozmenyMenu()
         {
             Console.Clear();
-            Console.WriteLine("Előző Pontszámok: ");
             Elozmenyek();
             Console.WriteLine("1. Vissza A Játékmenübe \t 2. Vissza A Főmenübe");
             switch (Console.ReadKey(true).Key)
@@ -74,14 +72,21 @@ namespace Wordle
 
         private static void Elozmenyek()
         {
-            string eleresiUt = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName;
+            string eleresiUt = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\Elozmenyek.csv";
 
             List<string> sorok = new List<string>();
-            sorok = File.ReadAllLines(eleresiUt + @"\Ranglista.txt").ToList();
-            foreach (string sor in sorok)
+            sorok = File.ReadAllLines(eleresiUt).ToList();
+            if (sorok.Count != 0)
             {
-                Console.WriteLine(sor);
+                Console.WriteLine("Előző Pontszámok:");
+
+                foreach (string sor in sorok)
+                {
+                    Console.WriteLine(string.Join(" ", sor.Split(';')));
+                }
             }
+            else
+                Console.WriteLine("Még nincs lejátszott mértkőzés!");
         }
 
         private static void Jatek()
@@ -136,30 +141,46 @@ namespace Wordle
             for (int jatekosProba = 0; jatekosProba < 5 && korSzam <= 3; jatekosProba++)
             {
                 jatekosSzo = BekerUjSzo(jatekosSzo);
-                Console.WriteLine(mostaniSzo, mostaniSzo.Length); // csak tesztelesre
-                if (EltalaltaE(jatekosSzo, mostaniSzo) == true)
+                // Console.WriteLine(mostaniSzo, mostaniSzo.Length); // csak tesztelesre
+                if (EltalaltaE(jatekosSzo, mostaniSzo, 5 - jatekosProba) == true) // HA nyeri a kört
                 {
                     pontSzam += 5 - jatekosProba;
                     if (korSzam != 3)
-                        Console.WriteLine($"{korSzam}. kör nyert! // {jatekosProba} hiba a körben // {pontSzam} pont");
-                    else
                     {
-                        Console.WriteLine($"{korSzam}. kör nyert! // {pontSzam} pont // Nyomjon meg egy gombot a továbblépéshez");
-                        Console.ReadKey();
-
-                        string eleresiUt = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\Ranglista.txt";
-
-                        List<string> sorok = new List<string>();
-                        sorok = File.ReadAllLines(eleresiUt).ToList();
-                        sorok.Add($"{pontSzam} Pont // [név] //{DateTime.Now}"); // player neve kell majd ide !!!!!
-                        File.WriteAllLines(eleresiUt, sorok);
+                        Console.WriteLine($"{korSzam}. kör nyert! // {pontSzam} pont");
                     }
 
+                    else // ha az utolsó kör
+                    {
+                        SaveScore(pontSzam);
+                        Console.WriteLine($"{korSzam}. kör nyert! // Nyomjon meg egy gombot a továbblépéshez");
+                        Console.ReadKey();
+                    }
                     korSzam++;
                     mostaniSzo = UjSzo(szavak, r);
-                    jatekosProba = -1; // ha eltalalja megnoveli a korszamot, uj szót ker, és 5 uj hiba lehetoseget kap
+                    jatekosProba = -1; // megnoveli a korszamot, uj szót ker, és 5 uj hiba lehetoseget kap
+                }
+                else if (jatekosProba == 4)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Nem találta el 5 próba alatt!");
+                    Console.ResetColor();
+                    Console.WriteLine($"Nyomjon meg egy gombot a következő körhöz! // {pontSzam} pont");
+                    Console.ReadKey(true);
+                    mostaniSzo = UjSzo(szavak, r);
+                    jatekosProba = -1;
                 }
             }
+        }
+
+        private static void SaveScore(int pontSzam)
+        {
+            string eleresiUt = Directory.GetParent(Environment.CurrentDirectory).Parent.FullName + @"\Elozmenyek.csv";
+
+            List<string> sorok = new List<string>();
+            sorok = File.ReadAllLines(eleresiUt).ToList();
+            sorok.Add($"Pont: {pontSzam};Név: [név];Idő: {DateTime.Now}"); // player neve kell majd ide !!!!!
+            File.WriteAllLines(eleresiUt, sorok);
         }
 
         private static string BekerUjSzo(string jatekosSzo)
@@ -169,7 +190,11 @@ namespace Wordle
                 Console.Write("Tipp (5 karakter): ");
                 string input = Console.ReadLine();
                 if (input.Length != 5)
-                    Console.WriteLine("nem 5 karakter hosszu");
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("A megadott szó nem 5 karakter hosszú");
+                    Console.ResetColor();
+                }
                 jatekosSzo = input;
             } while (jatekosSzo.Length != 5);
 
@@ -181,7 +206,7 @@ namespace Wordle
             return szavak[r.Next(szavak.Length)];
         }
 
-        private static bool EltalaltaE(string jatekosSzo, string mostaniSzo)
+        private static bool EltalaltaE(string jatekosSzo, string mostaniSzo, int probaMaradt)
         {
             int betukJoHelyen = 0; // betuk ami jo helyen van (zold)
             int betukNemjoHelyen = 0; // betuk amik benne vannak a szoban de nem jo helyen (sarga)
@@ -190,7 +215,10 @@ namespace Wordle
                 if (jatekosSzo[i] == mostaniSzo[i]) // jo helyen van e a betu
                 {
                     betukJoHelyen++;
+                    Console.ForegroundColor = ConsoleColor.Green;
                     Console.WriteLine($"A(z) \"{jatekosSzo[i]}\" betű jó helyen van! ({i + 1}. karakter)");
+                    Console.ResetColor();
+                    Console.WriteLine($"{probaMaradt - 1} esély van még");
                 }
 
                 for (int j = 0; j < jatekosSzo.Length; j++)
@@ -198,7 +226,10 @@ namespace Wordle
                     if (jatekosSzo[i] == mostaniSzo[j] && jatekosSzo[i] != mostaniSzo[i])
                     {
                         betukNemjoHelyen++;
+                        Console.ForegroundColor = ConsoleColor.Yellow;
                         Console.WriteLine($"A(z) \"{jatekosSzo[i]}\" betű benne van de nem jó helyen van! ({i + 1}. karakter)");
+                        Console.ResetColor();
+                        Console.WriteLine($"{probaMaradt - 1} esély van még");
                     }
                 }
             }
